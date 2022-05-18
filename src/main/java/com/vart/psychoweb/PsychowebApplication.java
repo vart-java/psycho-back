abbd3ba408f8ffaf0ff2a4b7378aabb8c63decd1
Vitaliy A <vitaliy.a@dev.tagsoft.co>
2022-05-18 12:36:27 +0300

    initial commit

---
+++ src/main/java/com/vart/psychoweb/PsychowebApplication.java
@@ -0,0 +1,13 @@
+package com.vart.psychoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychowebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychowebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vart/psychoweb/PsychowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vart.psychoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychowebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
